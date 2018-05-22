b24fca5761716fe82c0d5fa7596f2406597356ad
Jake Luton <jake.luton92@gmail.com>
2018-05-22 16:35:12 -0700

    Stores all anagrams in a hash table, with sorted characters as key.

--- anagrams.js
+++ anagrams.js
@@ -1,18 +1,29 @@
 XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
 const createAnagramPrinter = function () {
+  const anagrams = {};
   // Generate Hash Map with all sorted letter combinations, associated with array of valid anagrams
+
+  const hashWord = function (word) {
+    const alphabetizedWord = word.toUpperCase().split('').sort().join('');
+    if (alphabetizedWord in anagrams) {
+      anagrams[alphabetizedWord].push(word);
+    } else {
+      anagrams[alphabetizedWord] = [word];
+    }
+    console.log(anagrams[alphabetizedWord]);
+  };
+
   const dictionary = new XMLHttpRequest();
   dictionary.open('GET', 'http://codekata.com/data/wordlist.txt');
-
   dictionary.setRequestHeader('Access-Control-Allow-Origin', '*');
   dictionary.setRequestHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
   dictionary.setRequestHeader('Access-Control-Allow-Headers', 'Origin, Content-Type, X-Auth-Token');
 
-
   dictionary.onreadystatechange = () => {
     if (dictionary.readyState === 4 && dictionary.status === 200) {
-      console.log(dictionary.responseText);
+      const dictionaryWords = dictionary.responseText.split('\n');
+      dictionaryWords.forEach(hashWord);
     }
   }
 
