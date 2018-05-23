71f2018b083d62c976c5a260a6e2f406ee8e67fa
Jake Luton <jake.luton92@gmail.com>
2018-05-22 17:14:34 -0700

    Refactors to use fetch API instead of XMLHTTPRequest in order to make use of promises.

--- anagrams.js
+++ anagrams.js
@@ -1,33 +1,71 @@
-XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+// XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+const fetch = require('node-fetch');
 
-const createAnagramPrinter = function () {
-  const anagrams = {};
-  // Generate Hash Map with all sorted letter combinations, associated with array of valid anagrams
+/* Fetches all words in dictionary, and stores them in a hash map associated with its alphabetically 
+sorted characters. Returns a promise that resolves to hash map. */
+const getAnagramsFromDictionary = function () {
+  return fetch('http://codekata.com/data/wordlist.txt')
+    .then(res => res.text())
+    .then(body => body.split('\n'))
+    .then((words) => {
+      const anagrams = {};
+      words.forEach((word) => {
+        const alphabetizedWord = word.toUpperCase().split('').sort().join('');
+        if (alphabetizedWord in anagrams) {
+          anagrams[alphabetizedWord].push(word);
+        } else {
+          anagrams[alphabetizedWord] = [word];
+        }
+      })
+      return anagrams;
+    })
 
-  const hashWord = function (word) {
-    const alphabetizedWord = word.toUpperCase().split('').sort().join('');
-    if (alphabetizedWord in anagrams) {
-      anagrams[alphabetizedWord].push(word);
-    } else {
-      anagrams[alphabetizedWord] = [word];
-    }
-    console.log(anagrams[alphabetizedWord]);
-  };
 
-  const dictionary = new XMLHttpRequest();
-  dictionary.open('GET', 'http://codekata.com/data/wordlist.txt');
-  dictionary.setRequestHeader('Access-Control-Allow-Origin', '*');
-  dictionary.setRequestHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
-  dictionary.setRequestHeader('Access-Control-Allow-Headers', 'Origin, Content-Type, X-Auth-Token');
 
-  dictionary.onreadystatechange = () => {
-    if (dictionary.readyState === 4 && dictionary.status === 200) {
-      const dictionaryWords = dictionary.responseText.split('\n');
-      dictionaryWords.forEach(hashWord);
-    }
+  // const dictionary = new XMLHttpRequest();
+  // dictionary.open('GET', 'http://codekata.com/data/wordlist.txt');
+  // dictionary.setRequestHeader('Access-Control-Allow-Origin', '*');
+  // dictionary.setRequestHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
+  // dictionary.setRequestHeader('Access-Control-Allow-Headers', 'Origin, Content-Type, X-Auth-Token');
+
+  // return new Promise((resolve, reject) => {
+  //   const anagrams = {};
+
+  //   dictionary.onreadystatechange = () => {
+  //     if (dictionary.readyState === 4) {
+  //       if (dictionary.status === 200) {
+  //         const dictionaryWords = dictionary.responseText.split('\n');
+
+  //         dictionaryWords.forEach((word) => {
+  //           const alphabetizedWord = word.toUpperCase().split('').sort().join('');
+  //           if (alphabetizedWord in anagrams) {
+  //             anagrams[alphabetizedWord].push(word);
+  //           } else {
+  //             anagrams[alphabetizedWord] = [word];
+  //           }
+  //         });
+
+  //         resolve(anagrams);
+  //       } else {
+  //         reject(new Error (`Dictionary request resolved with status code ${dictionary.status}`));
+  //       }
+  //     } 
+  //   }
+
+  //   dictionary.send();  
+  // });
+
+  
+};
+
+const createAnagramPrinter = async function () {
+
+  try {
+    const anagrams = await getAnagramsFromDictionary();
+    console.log(anagrams);
+  } catch (err) {
+    throw err;
   }
-
-  dictionary.send();
 
   return (filePath) => {
     // Reads .txt file,
