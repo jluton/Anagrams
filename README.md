b002164f740aeea917d2de5f106c04f2843336d5
Jake Luton <jake.luton92@gmail.com>
2018-05-22 21:08:32 -0700

    Refactors code. Exports printAnagrams, which reads a text file and prints anagrams if anagrams hash is ready.

--- README.md
+++ README.md
@@ -5,10 +5,8 @@
 2. Our anagram-printing function will operate by taking in the filepath to a local .txt file that contains the words for which we will find anagrams.
 
 #Notes on this design
-1. I decided to create the anagrams hash map and the printAnagrams function inside of a closure into order to privatize the anagrams variable.
-
-2. This approach words by iterating over the dictionary once to generate a hash map of all letter combinations and their associated anagrams. This allows us perform one expensive operation upfront so that printAnagrams can run with great time efficiency thereafter. 
+This approach words by iterating over the dictionary once to generate a hash map of all letter combinations and their associated anagrams. This allows us perform one expensive operation upfront so that printAnagrams can run with great time efficiency thereafter. 
 
 The downsides of this approach are A.) The initial generation of the Hash Map occurs in O(n) time, where n is the size of the dictionary. Given that n is large, this will be a time consuming operation. B.) The hash map will take up O(n) space. Given that n is large, this will take up quite a bit of memory.
 
-3. If I were implementing this algorithm as part of a real application, I would strongly consider storing the anagrams hash map in a NoSQL database rather than as an in-memory variable. This would reduce lookup speed but would significantly decrease memory usage.
+If I were implementing this algorithm as part of a real application, I would strongly consider storing the anagrams hash map in a NoSQL database rather than as an in-memory variable. This would reduce lookup speed but would significantly decrease memory usage.
--- anagrams.js
+++ anagrams.js
@@ -1,77 +1,53 @@
-// XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 const fetch = require('node-fetch');
+const fs = require('fs');
 
-/* Fetches all words in dictionary, and stores them in a hash map associated with its alphabetically 
-sorted characters. Returns a promise that resolves to hash map. */
-const getAnagramsFromDictionary = function () {
-  return fetch('http://codekata.com/data/wordlist.txt')
+let anagrams;
+
+/* Returns the characters of a word, forced to upper case and sorted alphabetically */
+const sortWordCharacters = function (word) {
+  return word.toUpperCase().split('').sort().join('');
+};
+
+/* Given a list of words, returns a hash table with arrays of valid anagrams associated with
+alphabetized characters */
+const generateAnagramsForListOfWords = function (words) {
+  anagrams = {};
+  words.forEach((word) => {
+    const alphabetizedWord = sortWordCharacters(word);
+    if (alphabetizedWord in anagrams) {
+      anagrams[alphabetizedWord].push(word);
+    } else {
+      anagrams[alphabetizedWord] = [word];
+    }
+  });
+  printAnagrams(__dirname + '/exampleInput.txt')
+};
+
+/* Fetches all words in dictionary, and splits them into an array */
+const getAnagramsFromDictionary = function (url = 'http://codekata.com/data/wordlist.txt') {
+  return fetch(url)
     .then(res => res.text())
     .then(body => body.split('\n'))
-    .then((words) => {
-      const anagrams = {};
-      words.forEach((word) => {
-        const alphabetizedWord = word.toUpperCase().split('').sort().join('');
-        if (alphabetizedWord in anagrams) {
-          anagrams[alphabetizedWord].push(word);
-        } else {
-          anagrams[alphabetizedWord] = [word];
-        }
-      })
-      return anagrams;
-    })
-
-
-
-  // const dictionary = new XMLHttpRequest();
-  // dictionary.open('GET', 'http://codekata.com/data/wordlist.txt');
-  // dictionary.setRequestHeader('Access-Control-Allow-Origin', '*');
-  // dictionary.setRequestHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
-  // dictionary.setRequestHeader('Access-Control-Allow-Headers', 'Origin, Content-Type, X-Auth-Token');
-
-  // return new Promise((resolve, reject) => {
-  //   const anagrams = {};
-
-  //   dictionary.onreadystatechange = () => {
-  //     if (dictionary.readyState === 4) {
-  //       if (dictionary.status === 200) {
-  //         const dictionaryWords = dictionary.responseText.split('\n');
-
-  //         dictionaryWords.forEach((word) => {
-  //           const alphabetizedWord = word.toUpperCase().split('').sort().join('');
-  //           if (alphabetizedWord in anagrams) {
-  //             anagrams[alphabetizedWord].push(word);
-  //           } else {
-  //             anagrams[alphabetizedWord] = [word];
-  //           }
-  //         });
-
-  //         resolve(anagrams);
-  //       } else {
-  //         reject(new Error (`Dictionary request resolved with status code ${dictionary.status}`));
-  //       }
-  //     } 
-  //   }
-
-  //   dictionary.send();  
-  // });
-
-  
+    .then(generateAnagramsForListOfWords)
+    .catch((err) => { throw err; });
 };
 
-const createAnagramPrinter = async function () {
-
-  try {
-    const anagrams = await getAnagramsFromDictionary();
-    console.log(anagrams);
-  } catch (err) {
-    throw err;
+/* Reads .txt file, and prints anagrams to the console */
+const printAnagrams = function (filePath = __dirname + '/exampleInput.txt') {
+  if (!anagrams) {
+    throw new Error('Cannot print anagrams until anagrams hash table is ready!');
   }
 
-  return (filePath) => {
-    // Reads .txt file,
-    // For each line in the .txt file, coerce to uppercase and sort alphabetically.
-    // Access sorted word in hash map, print contents of associated array.
-  }
-};
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    const words = data.split('\n');
+    words.forEach((word) => {
+      const alphabetizedWord = sortWordCharacters(word);
+      console.log(...anagrams[alphabetizedWord]);
+    });
+  });
+}
 
-const printAnagrams = createAnagramPrinter();
+getAnagramsFromDictionary();
+
+module.exports.printAnagrams = printAnagrams;
+
